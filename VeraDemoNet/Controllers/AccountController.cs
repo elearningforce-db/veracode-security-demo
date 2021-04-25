969491c6722828eb54b6fb932936b00265dc740e
Denis Bobrov <dbobrov@lms365.com>
2021-04-26 01:14:23 +0300

    14-cwe-117-improper-output-neutralization-for-logs FIX

--- VeraDemoNet/Controllers/AccountController.cs
+++ VeraDemoNet/Controllers/AccountController.cs
@@ -36,7 +36,7 @@ namespace VeraDemoNet.Controllers
         [HttpGet, ActionName("Login")]
         public ActionResult GetLogin(string ReturnUrl = "")
         {
-            logger.Info("Login page visited: " + ReturnUrl);
+            logger.Info("Login page visited");
 
             if (IsUserLoggedIn())
             {
@@ -90,7 +90,7 @@ namespace VeraDemoNet.Controllers
                     using (EventLog eventLog = new EventLog("Application"))
                     {
                         eventLog.Source = "Application";
-                        eventLog.WriteEntry("Entering PostLogin with target " + ReturnUrl + " and username " + loginViewModel.UserName, EventLogEntryType.Information, 101, 1);
+                        eventLog.WriteEntry("Entering PostLogin with username " + loginViewModel.UserName, EventLogEntryType.Information, 101, 1);
                     }
 
                     if (userDetails == null)

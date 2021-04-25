f700b2f64a4d06aa772046d1b04bbbb1f0eef505
Denis Bobrov <dbobrov@lms365.com>
2021-04-26 00:26:45 +0300

    12-cwe-384-session-fixation FIX

--- VeraDemoNet/Controllers/AuthControllerBase.cs
+++ VeraDemoNet/Controllers/AuthControllerBase.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
+using System.Web;
 using System.Web.Mvc;
 using VeraDemoNet.DataAccess;
 
@@ -41,7 +42,7 @@ namespace VeraDemoNet.Controllers
 
         protected void LogoutUser()
         {
-            Session["username"] = null;
+            Response.Cookies.Add(new HttpCookie("ASP.NET_SessionId", ""));
         }
 
         protected bool IsUserLoggedIn()
